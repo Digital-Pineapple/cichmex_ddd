b76c92ec47f83eb52ce9d6da63032f9cd9c83b83
Rodrigo Peñaloza <rodrigorpf.dp@gmail.com>
2024-08-15 20:59:22 -0600

    reference info

--- src/api/infrastructure/controllers/paymentsController/paymentController.ts
+++ src/api/infrastructure/controllers/paymentsController/paymentController.ts
@@ -86,9 +86,6 @@ export class PaymentController extends ResponseData {
 
     public async createPaymentMP(req: Request, res: Response, next: NextFunction) {
         const { membership, user, values } = req.body;
-        console.log(req.body);
-        
-    
 
         const access_token = config.MERCADOPAGO_TOKEN;
         const client = new MercadoPagoConfig({ accessToken: access_token, options: { timeout: 5000 } });
@@ -97,7 +94,7 @@ export class PaymentController extends ResponseData {
 
         try {
             // Crea un pago en la base de datos
-            const response1 = await this.paymentUseCase.createNewPayment({ uuid: uuid4 });
+            const response1 : any = await this.paymentUseCase.createNewPayment({ uuid: uuid4 });
 
             const path_notification = `${process.env.URL_NOTIFICATION}api/payments/Mem-Payment-success`;
 
@@ -146,16 +143,16 @@ export class PaymentController extends ResponseData {
                         });
                         const client_id = user_id;
                         const idMembership = membership._id
-                        const membership_info = await this.membershipUseCase.getInfoMembership(idMembership);
+                        const membership_info : any = await this.membershipUseCase.getInfoMembership(idMembership);
                         if (!(membership_info instanceof ErrorHandler)) {
                             let start_date1 = moment().format('L');
                             let end_date1 = moment().add(30, 'days').calendar();
                             let services = membership_info?.service_quantity;
                             let mem_id = membership_info?.id
                             if (services !== undefined) {
-                                await Promise.all(services.map(async (item) => {
+                                await Promise.all(services.map(async (item : any ) => {
                                     try {
-                                        const ok = await this.membershipBenefitUseCAse.createNewMembershipBenefit(
+                                        const ok : any = await this.membershipBenefitUseCAse.createNewMembershipBenefit(
                                             mem_id,
                                             item.service_id._id,
                                             client_id,
@@ -335,7 +332,7 @@ export class PaymentController extends ResponseData {
     
         try {
             // Create a payment in the database
-            const response1 = await this.paymentUseCase.createNewPayment({
+            const response1 : any = await this.paymentUseCase.createNewPayment({
                 uuid: uuid4,
                 user: user._id,
                 payment_status: 'pending',
@@ -386,7 +383,7 @@ export class PaymentController extends ResponseData {
     public async createTicket(req: Request, res: Response, next: NextFunction) {
 
         try {
-            const info = await this.mpService.reciveWebHook(req);
+            const info : any = await this.mpService.reciveWebHook(req);
 
             if (info !== undefined && info.status === 'approved') {
 
@@ -403,7 +400,7 @@ export class PaymentController extends ResponseData {
                         if (services !== undefined) {
                             await Promise.all(services.map(async (item) => {
                                 try {
-                                    const ok = await this.membershipBenefitUseCAse.createNewMembershipBenefit(
+                                    const ok : any = await this.membershipBenefitUseCAse.createNewMembershipBenefit(
                                         mem_id,
                                         item.service_id._id,
                                         id_client,
