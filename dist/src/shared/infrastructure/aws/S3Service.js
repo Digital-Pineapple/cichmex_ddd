088ebd820ed58b4be6b1c045e9bf1be538955c6a
RodrigoRPF <rodrigorpf.dp.com>
2024-10-31 13:19:18 -0600

    feed products and seed products

--- dist/src/shared/infrastructure/aws/S3Service.js
+++ dist/src/shared/infrastructure/aws/S3Service.js
@@ -36,7 +36,7 @@ class S3Service {
                 const params = {
                     Bucket: this.bucket,
                     Key: this.environment + key,
-                    ContentType: contentType, //
+                    ContentType: contentType,
                     Body: fileContent,
                 };
                 yield this.s3.upload(params).promise();
@@ -65,7 +65,7 @@ class S3Service {
             const params = {
                 Bucket: this.bucket,
                 Key: this.environment + key,
-                // Expires : 300,
+                Expires: 25 * 60 * 60,
             };
             return yield this.s3.getSignedUrl('getObject', params);
         });
