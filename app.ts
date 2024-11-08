5cdc969d059438b0daf3ea8adc20d82a490abae4
hugonavadp <hugo-nava@digital-pineapple.com.mx>
2024-11-08 16:45:22 -0600

    app.ts

--- app.ts
+++ app.ts
@@ -2,17 +2,20 @@ import { Configuration } from './config';
 import { Container } from './src/shared/infrastructure/Container';
 
 import { Server } from './src/shared/infrastructure/server/Server';
+// import { whatsappService } from './src/shared/infrastructure/whatsapp/WhatsappService..external';
+
 
 const container = new Container();
 const server = container.invoke().resolve<Server>('server');
 const config = container.invoke().resolve<Configuration>('config');
 
+
 server
     .startServer()
     .then(async () => {
         await container.invoke().resolve('db')
-        console.log(`Env: ${config.NODE_ENV}`);
-
+        console.log(`Env: ${config.NODE_ENV}`);          
+        // console.log(`S3: ${process.env.S3_ENVIRONMENT}`);              
     }).catch((err)=>{
         console.error('Error starting server:', err);
     });
