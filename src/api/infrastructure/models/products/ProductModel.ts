97d0f074a0ecb182b772d96633994447e0b300ae
RodrigoRPF <rodrigorpf.dp.com>
2024-12-27 11:55:27 -0600

    create product ok

--- src/api/infrastructure/models/products/ProductModel.ts
+++ src/api/infrastructure/models/products/ProductModel.ts
@@ -53,7 +53,7 @@ const ProductSchema = new Schema<ProductEntity>(
     name: {
       type: String,
       required: true,
-      unique:true  
+      unique:false,
     },
     price: {
       type: Number,
