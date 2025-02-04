dc376436e07f8890f2a2c4cf24985e5194d38320
RodrigoRPF <rodrigorpf.dp.com>
2025-02-04 11:57:33 -0600

    auth admin

--- app.ts
+++ app.ts
@@ -4,13 +4,13 @@ import { backUpDBToS3 } from './src/helpers/backUpDB';
 import { Container } from './src/shared/infrastructure/Container';
 import { Server } from './src/shared/infrastructure/server/Server';
 // import { whatsappService } from './src/shared/infrastructure/whatsapp/WhatsappService..external';
-const time = '0 0 * * *' // cada dia
-cron.schedule(time, async () => {
-    if(config.NODE_ENV.toLocaleLowerCase() === "production"){
-        await backUpDBToS3();
-    }
-    return;
-});
+// const time = '0 0 * * *' // cada dia
+// cron.schedule(time, async () => {
+//     if(config.NODE_ENV.toLocaleLowerCase() === "production"){
+//         await backUpDBToS3();
+//     }
+//     return;
+// });
 
 const container = new Container();
 const server = container.invoke().resolve<Server>('server');
