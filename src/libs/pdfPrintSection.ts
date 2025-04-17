5eee249b67a648c2394ae454f7c8b648a30642c9
RodrigoRPF <rodrigorpf.dp.com>
2025-04-17 09:35:35 -0600

    print qr section

--- src/libs/pdfPrintSection.ts
+++ src/libs/pdfPrintSection.ts
@@ -19,14 +19,14 @@ export function buildReportSectionPDF(section: any, dataCallback: any, endCallba
     // const qrData = JSON.stringify({ order_id: orderData.order_id });
     // const qrImage = qr.imageSync(qrData, { type: 'png' });
     const writeStream = fs.createWriteStream('output.pdf');
-    const imagePath = path.join(__dirname, '../shared/assets/CHMX/Imagotipo CHMX Negro.png');
+    // const imagePath = path.join(__dirname, '.../shared/assets/CHMX/Imagotipo CHMX Negro.png');
     doc.pipe(writeStream);
 
     const qrBuffer = qr.imageSync(JSON.stringify(_id), { type: 'png' });
 
 
 
-    doc.image(imagePath, 450, doc.y - 20, { width: 80, align: 'right' });
+    // doc.image(imagePath, 450, doc.y - 20, { width: 80, align: 'right' });
     doc.image(qrBuffer, column2X, currentY, { width: 200, align: 'center' });
     // Primera columna
     doc.fontSize(20);
